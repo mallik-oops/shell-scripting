540c5e56577788c053de2ce7a5a29b847d2baee7
shiva <mallikarjun.sanj@gmail.ccom>
2025-02-03 12:57:56 +0530

    changes happend to file

---
+++ 02-variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "ram:: Hi robert"
+echo "robert: hello ram"
+echo "ram: hov are you doing?"
+echo "robert : I am good, Hov are you?"
---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Please enter your username"
+
+read USERNAME
+
+echo "username entered: $USERNAME"
+
+read -s PASSWORD
