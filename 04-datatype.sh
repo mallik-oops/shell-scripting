90a05de55375eab83e30fe9d8205c81ebbcbbcef
shiva <mallikarjun.sanj@gmail.ccom>
2025-02-04 12:41:41 +0530

    addition script

--- 04-datatype.sh
+++ 04-datatype.sh
@@ -3,7 +3,9 @@ NUMBER1=$1
 NUMBER2=$2
 
 TIMESTAMP=$(date)
-echo "Script executed at " $TIMESTAMP
-SUM=$(($NUMNER1+NUMBER2))
+echo "Script executed at " $TIMESTAMP"
+
+SUM=$(($NUMBER1+$NUMBER2))
+
 
 echo "SUM of $Number1 and $NUMBER2 is: $SUM"
