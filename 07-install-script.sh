1c95b4ca3f059d93b1c4a08b37cd9b40ef591d9f
shiva <mallikarjun.sanj@gmail.ccom>
2025-02-05 10:54:18 +0530

    installation script got updated

--- 07-install-script.sh
+++ 07-install-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USERID=$(id -u)
-if [ $USERID -ne 0]
+if [ $USERID -ne 0 ]
 then
     echo "ERROR:: You must have sudo acess to excute this script"
     exit 1 #other than 0
@@ -20,7 +20,7 @@ fi
 
 dnf install git -y
 
-if [ $? -ne 0]
+if [ $? -ne 0 ]
 then 
     echo "Installing Git....FAILURE"
     exit 1
