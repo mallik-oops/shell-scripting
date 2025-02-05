6dd592b8b895ae82b4a47834b141da49e22d337d
shiva <mallikarjun.sanj@gmail.ccom>
2025-02-05 06:34:50 +0530

    condition statement

---
+++ 06.condition.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+NUMBER=$1
+
+# -gt, -lt, -ge ,le 
+
+if [ $NUMBER -gt 100 ]
+
+then 
+    echo "Given number is greater than 100"
+else
+    echo "Given Number is less than or equal to 100"
+
+fi
